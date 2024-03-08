ae5a22931be18acf58699bef9a961a0cfa07117f
Mateo-Moretti <mateomore@gmail.com>
2024-03-08 19:44:23 -0300

    Base models classes: User, Student, Profesor, Course

---
+++ UniversitySystem.Contracts/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversitySystem.Contracts.Models
+{
+    public class Student : User
+    {
+        public Student(int id, string name, string surname, string email, string password, int phoneNumber) : base(id, name, surname, email, password, phoneNumber)
+        {
+        }
+    }
+}
--- UniversitySystem.Contracts/Models/User.cs
+++ UniversitySystem.Contracts/Models/User.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace UniversitySystem.Contracts.Models
 {
-    internal class User
+    public abstract class User
     {
+        public int id;
+        public string Name;
+        public string Surname;
+        public string Email;
+        public string Password;
+        public int PhoneNumber;
+
+        public User(int id, string name, string surname, string email, string password, int phoneNumber)
+        {
+            this.id = id;
+            this.Name = name;
+            this.Surname = surname;
+            this.Email = email;
+            this.Password = password;
+            this.PhoneNumber = phoneNumber;
+        }
     }
 }
